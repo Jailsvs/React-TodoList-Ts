ff765eecbbf1818d15e8ac6ea496e45b4720c87b
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-11-30 07:52:07 -0300

    Criação do projeto com componentes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+//css
+import './App.css'
+import styles from './App.module.css';
+
+//components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  
+  return (
+    <div>
+      <Header/>
+      <main className={styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p><span>TODO React + TS</span> @ 2021</p>
+    </footer>
+  )
+}
+
+export default Footer
