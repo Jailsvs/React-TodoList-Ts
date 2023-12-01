da71819708629e740e506e91b75e4335ba73f9e7
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-01 07:37:15 -0300

    Form de tasks

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import styles from './App.module.css';
 //components
 import Header from './components/Header'
 import Footer from './components/Footer'
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   
@@ -12,7 +14,14 @@ function App() {
     <div>
       <Header/>
       <main className={styles.main}>
-        <h1>Conteúdo...</h1>
+        <div>
+          <h2>O que vc vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa"/>
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList/>
+        </div>
       </main>
       <Footer/>
     </div>
